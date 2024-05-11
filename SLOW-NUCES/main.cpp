427bdc2ab0a31e7bf7bf951d15ab8dcf66e8c702
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-05-11 10:21:29 -0700

    added pngs

--- SLOW-NUCES/main.cpp
+++ SLOW-NUCES/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1250, 800), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
